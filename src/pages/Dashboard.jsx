614b39f138f3fa6dbed9d82d2b2ec95e98083e67
ovwyz <oscar.zepvaz@gmail.com>
2025-06-06 12:56:50 -0600

    Enhance pagination controls and add download functionality in Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,8 @@
+import { apiFetch } from "@/utils/apiFetch";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { Icon } from "@iconify/react";
+import { showToast } from "@/utils/toastUtils";
 import TotalRequests from "@/features/admin/dashboard/components/cards/TotalRequests";
 import RequestsStatus from "@/features/admin/dashboard/components/charts/RequestsStatus";
 import RequestsByTech from "@/features/admin/dashboard/components/charts/RequestsByTech";
@@ -11,11 +15,47 @@ import TopActiveUsers from "@/features/admin/dashboard/components/charts/TopActi
 import FrequentHours from "@/features/admin/dashboard/components/charts/FrequentHours";
 
 function Dashboard() {
+    const handleDownload = async () => {
+        try {
+            const data = await apiFetch("/export/graph", {
+                method: "POST",
+                responseType: "blob", // 👈 Esto activa `isBlob` en tu apiFetch.js
+                body: JSON.stringify({ period: 1, month: 8, year: 2025 }),
+            });
+
+            const url = window.URL.createObjectURL(data);
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = "Graficas_Cicata.xlsx";
+
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(url);
+        } catch (error) {
+            showToast("Error al exportar gráficas", "error");
+            console.error(error);
+        }
+    };
+
     return (
         <section className="p-2 -mt-1 w-full max-w-full overflow-x-hidden overflow-y-hidden pb-15">
-            <h2 className="font-poppins text-2xl font-bold mb-2">
-                Estado general
-            </h2>
+            <div className="flex justify-between mb-2">
+                <h2 className="font-poppins text-2xl font-bold">
+                    Estado general
+                </h2>
+                <Button
+                    onClick={handleDownload}
+                    className="bg-deep-blue hover:bg-dark-blue text-white text-sm font-poppins font-semibold py-2 px-4 rounded-md transition inline-flex items-center"
+                    aria-label="Descargar inventario"
+                >
+                    <Icon
+                        icon="material-symbols:download"
+                        className="mr-2 text-xl"
+                    />
+                    Descargar gráficas
+                </Button>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 mb-8">
                 <TotalRequests />
                 <RequestsStatus />
